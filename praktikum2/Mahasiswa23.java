90fce084ee2fe0ed1b514d8276d14717038d324b
vanesa <paneja436@gmail.com>
2025-02-21 08:42:41 +0700

    feat before update

---
+++ praktikum2/Mahasiswa23.java
@@ -0,0 +1,37 @@
+package praktikum2; //memberitahu dimana letak file
+
+public class Mahasiswa23 {
+
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi(){
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+       ipk = ipkBaru;
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.5) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        }
+    }
+
+}
