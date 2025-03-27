f4f91c0cf9ce19a4370df08769ec41c7d0b7c39a
vanesa <paneja436@gmail.com>
2025-03-27 22:35:22 +0700

    new

--- jobsheet6/Sorting23.java
+++ jobsheet6/Sorting23.java
@@ -2,49 +2,58 @@ package jobsheet6;
 
 public class Sorting23 {
 
-    int [] data20;
-    int jumData20;
+    int [] data23;
+    int jumData23;
 
     Sorting23 (int data[], int jmlDat) {
-        jumData20=jmlDat;
-        data20=new int [jmlDat];
-        for (int i=0; i<jumData20; i++) {
-            data20[i]= data[i];
+        jumData23=jmlDat;
+        data23=new int [jmlDat];
+        for (int i=0; i<jumData23; i++) {
+            data23[i]= data[i];
         }
     } 
 
     void bubbleSort() {
         int temp = 0;
-        for (int i=0; i<jumData20-1; i++){
-            for (int j=1; j<jumData20-i; j++) {
-                if (data20[j-1]>data20[j]){
-                    temp=data20[j];
-                    data20[j]=data20[j-1];
-                    data20[j-1]=temp;
+        for (int i=0; i<jumData23-1; i++){
+            for (int j=1; j<jumData23-i; j++) {
+                if (data23[j-1]>data23[j]){
+                    temp=data23[j];
+                    data23[j]=data23[j-1];
+                    data23[j-1]=temp;
                 }
             }
         }
     }
 
     void tampil() {
-        for (int i=0; i<jumData20; i++) {
-            System.out.print(data20[i]+" ");
+        for (int i=0; i<jumData23; i++) {
+            System.out.print(data23[i]+" ");
         }
         System.out.println();
     }
 
     void SelectionSort(){
-        for (int i=0; i<jumData20; i++){
+        for (int i=0; i<jumData23; i++){
             int min=i;
-            for (int j=i+1; j<jumData20; j++){
-                if(data20[j]<data20[min]){
+            for (int j=i+1; j<jumData23; j++){
+                if(data23[j]<data23[min]){
                     min=j;
                 }
             }
-            int temp=data20[i];
-            data20[i]=data20[min];
-            data20[min]=temp;
+            int temp=data23[i];
+            data23[i]=data23[min];
+            data23[min]=temp;
         }
     }
-    
+    void insertionSort(){
+        for (int i=1; i<=data23.length-1; i++){
+            int temp=data23[i];
+            int j=i-1;
+            while (j>=0 && data23[j]>temp){
+                data23[j+1]=data23[j];
+                j--;
+            }
+        }
+    }
 }
