a499f9f92a0fe71f78b9624a6d5dd944b9e16110
vanesa <paneja436@gmail.com>
2025-03-27 22:31:42 +0700

    new

--- jobsheet6/Sorting23.java
+++ jobsheet6/Sorting23.java
@@ -5,11 +5,11 @@ public class Sorting23 {
     int [] data20;
     int jumData20;
 
-    Sorting23 (int Data[], int jmlDat) {
+    Sorting23 (int data[], int jmlDat) {
         jumData20=jmlDat;
         data20=new int [jmlDat];
         for (int i=0; i<jumData20; i++) {
-            data20[i]= Data[i];
+            data20[i]= data[i];
         }
     } 
 
@@ -32,4 +32,19 @@ public class Sorting23 {
         }
         System.out.println();
     }
+
+    void SelectionSort(){
+        for (int i=0; i<jumData20; i++){
+            int min=i;
+            for (int j=i+1; j<jumData20; j++){
+                if(data20[j]<data20[min]){
+                    min=j;
+                }
+            }
+            int temp=data20[i];
+            data20[i]=data20[min];
+            data20[min]=temp;
+        }
+    }
+    
 }
--- jobsheet6/SortingMain23.java
+++ jobsheet6/SortingMain23.java
@@ -4,12 +4,21 @@ public class SortingMain23 {
 
     public static void main(String[] args) {
         int a[]= {20, 10, 2, 7, 12};
+        int b[]= {30, 20,2, 8, 14};
+
         Sorting23 dataurut1 = new Sorting23(a, a.length);
         System.out.println("Data awal 1: ");
         dataurut1.tampil();
         dataurut1.bubbleSort();
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC):  ");
         dataurut1.tampil();
+
+        Sorting23 dataurut2 = new Sorting23(b, b.length);
+        System.out.println("Data awal 2: ");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC): ");
+        dataurut2.tampil();
 
     }
 }
