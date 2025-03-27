39b6a4b9035421d54344afe8fdac97a31659d332
vanesa <paneja436@gmail.com>
2025-03-27 22:36:55 +0700

    new

--- jobsheet6/SortingMain23.java
+++ jobsheet6/SortingMain23.java
@@ -5,6 +5,7 @@ public class SortingMain23 {
     public static void main(String[] args) {
         int a[]= {20, 10, 2, 7, 12};
         int b[]= {30, 20,2, 8, 14};
+        int c[]= {40, 10, 4, 9, 3};
 
         Sorting23 dataurut1 = new Sorting23(a, a.length);
         System.out.println("Data awal 1: ");
@@ -19,6 +20,13 @@ public class SortingMain23 {
         dataurut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC): ");
         dataurut2.tampil();
+
+        Sorting23 dataurut3 = new Sorting23(c, c.length);
+        System.out.println("Data awal 3: ");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERT SORT (ASC)");
+        dataurut3.tampil();
 
     }
 }
