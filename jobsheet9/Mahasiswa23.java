0a481153472bf1a07bef17da6d17893cede103a5
vanesa <paneja436@gmail.com>
2025-05-18 13:21:46 +0700

    new

---
+++ jobsheet9/Mahasiswa23.java
@@ -0,0 +1,21 @@
+package jobsheet9;
+
+public class Mahasiswa23 {
+    String nama;
+    String nim;
+    String kelas;
+    double nilai;
+
+    Mahasiswa23() {
+    }
+
+    Mahasiswa23(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+    }
+}
---
+++ jobsheet9/StackTugasMahasiswa23.java
@@ -0,0 +1,65 @@
+package jobsheet9;
+
+public class StackTugasMahasiswa23 {
+    Mahasiswa23[] stack;
+    int top;
+    int size;
+
+    public StackTugasMahasiswa23(int size) {
+        this.size = size;
+        stack = new Mahasiswa23[size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(Mahasiswa23 mhs) {
+        if (isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
+
+    public Mahasiswa23 pop() {
+        if (!isEmpty()) {
+            Mahasiswa23 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+
+    public Mahasiswa23 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return null;
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+}
