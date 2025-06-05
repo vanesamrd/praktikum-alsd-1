f142e38bea71d3764c8240e11c19591eda15ed10
vanesa <paneja436@gmail.com>
2025-06-05 09:07:26 +0700

    percobaan

--- jobsheet12/DLLmain23.java
+++ jobsheet12/DLLmain23.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 
 public class DLLmain23 {
+    static Mahasiswa23 inputMahasiswa(Scanner scan) {
+        System.out.print("Masukkan NIM: ");
+        String nim = scan.nextLine();
+        System.out.print("Masukkan Nama: ");
+        String nama = scan.nextLine();
+        System.out.print("Masukkan Kelas: ");
+        String kelas = scan.nextLine();
+        System.out.print("Masukkan IPK: ");
+        double ipk = scan.nextDouble();
+        return new Mahasiswa23(nama, nim, kelas, ipk);
+    }
   public static void main(String[] args) {
     DoubleLinkedList23 list = new DoubleLinkedList23();
     Scanner scan = new Scanner(System.in);
@@ -19,13 +30,33 @@ public class DLLmain23 {
       System.out.print("Pilih Menu : ");
       pilihan = scan.nextInt();
       scan.nextLine();
-
+        
       switch (pilihan) {
         case 1 -> {
             Mahasiswa23 mhs = inputMahasiswa(scan);
             list.addFirst(mhs);
         }
+        case 2 -> {
+            Mahasiswa23 mhs = inputMahasiswa(scan);
+            list.addLast(mhs);
+        }
+        case 3 -> list.removeFirst();
+        case 4 -> list.removeLast();
+        case 5 -> list.print();
+        case 6 -> {
+            System.out.print("Masukkan NIM yang dicari: ");
+            String nim = scan.nextLine();
+            Node23 found = list.search(nim);
+            if (found != null) {
+                System.out.println("Data ditemukan:");
+                found.data.tampil();
+            } else {
+                System.out.println("Data tidak ditemukan.");
+            }
+        }
       }
-    } while
+      
+    } while(pilihan !=0);
+    scan.close();
   }
 }
